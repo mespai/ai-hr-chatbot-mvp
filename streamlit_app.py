a8c203b9503f0de77e1561c6d02c10fc42bcdf08
mespai <omar@mespai.com>
2025-06-03 20:37:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,14 +23,27 @@ def is_valid_domain(email):
 # --- Connect to Google Sheets ---
 def connect_to_sheets(sheet_name):
     SCOPES = [
-        "https://www.googleapis.com/auth/spreadsheets",
+        "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    
+
     try:
-        # Try to use Streamlit secrets first (for cloud deployment)
+        # Prepare credentials dictionary for Streamlit secrets
+        credentials_dict = {
+            "type": st.secrets["type"],
+            "project_id": st.secrets["project_id"],
+            "private_key_id": st.secrets["private_key_id"],
+            "private_key": st.secrets["private_key"].replace('\\n', '\n'),
+            "client_email": st.secrets["client_email"],
+            "client_id": st.secrets["client_id"],
+            "auth_uri": st.secrets["auth_uri"],
+            "token_uri": st.secrets["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+        }
+
         credentials = Credentials.from_service_account_info(
-            st.secrets["gcp_service_account"],
+            credentials_dict,
             scopes=SCOPES
         )
     except:
@@ -40,7 +53,7 @@ def connect_to_sheets(sheet_name):
             SERVICE_ACCOUNT_FILE,
             scopes=SCOPES
         )
-    
+
     client = gspread.authorize(credentials)
     sheet = client.open(sheet_name).sheet1
     return sheet
